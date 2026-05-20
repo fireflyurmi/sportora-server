b603aa26a636940468972af4ce7fcfdbdceca092
unknown <halderurmi@gmail.com>
2026-05-20 14:47:39 +0600

    facilities post api has created

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require('express')
 const dotenv = require('dotenv');
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 const uri = process.env.MONGODB_URI;
 const app = express()
 const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,10 +23,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const db = client.db("sportora");
+    const facilitycollection = db.collection("facilities");
+    const bookingCollection = db.collection("bookings");
+
+    app.post('/facility', async (req, res)=>{
+        const facilityData = req.body
+        console.log(facilityData);
+        const result = await facilitycollection.insertOne(facilityData);
+
+        res.json(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
