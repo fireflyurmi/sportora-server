a0633df556e67f5a3f04171cb6ac72015310ef08
unknown <halderurmi@gmail.com>
2026-05-21 14:51:10 +0600

    update facilitiy function has done

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 dotenv.config();
 
 const uri = process.env.MONGODB_URI;
 const app = express();
 const PORT = process.env.PORT;
+
 app.use(cors());
 app.use(express.json());
 
@@ -18,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,21 +28,24 @@ async function run() {
 
     const db = client.db("sportora");
     const facilitycollection = db.collection("facilities");
-    const bookingCollection = db.collection("bookings");
 
-
-    app.get('/facility', async (req, res) => {
+    
+    app.get("/facility", async (req, res) => {
       try {
-        const { search, sportType } = req.query;
+        const { search, sportType, owner_email } = req.query;
         let query = {};
+
+        if (owner_email) {
+          query.$or = [
+            { owner_email: owner_email },
+            { creatorEmail: owner_email },
+            { email: owner_email },
+            { userEmail: owner_email },
+          ];
+        }
 
         if (search) {
           query.name = { $regex: search, $options: "i" };
-        }
-
-        if (sportType) {
-          const typesArray = sportType.split(",");
-          query.facility_type = { $in: typesArray };
         }
 
         const result = await facilitycollection.find(query).toArray();
@@ -52,32 +56,69 @@ async function run() {
       }
     });
 
-    app.post('/facility', async (req, res) => {
-      const facilityData = req.body;
-      console.log(facilityData);
-      const result = await facilitycollection.insertOne(facilityData);
-
-      res.json(result);
-    });
-
-
-    app.get('/facility/:id', async (req, res)=>{
-        const {id} = req.params
-        const result = await facilitycollection.findOne({_id: new ObjectId(id)});
-
+    
+    app.post("/facility", async (req, res) => {
+      try {
+        const facilityData = req.body;
+        const result = await facilitycollection.insertOne(facilityData);
         res.json(result);
+      } catch (error) {
+        console.error("Error creating facility:", error);
+        res
+          .status(500)
+          .json({ error: "Failed to create facility profile document" });
+      }
     });
 
+    
+    app.get("/facility/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const result = await facilitycollection.findOne({
+          _id: new ObjectId(id),
+        });
+        res.json(result);
+      } catch (error) {
+        console.error("Error reading single facility:", error);
+        res.status(500).json({ error: "Failed to read facility data" });
+      }
+    });
+
+    
+    app.patch("/facility/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const updatedData = req.body;
+        const result = await facilitycollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: updatedData },
+        );
+        res.json(result);
+      } catch (error) {
+        console.error("Error updating facility profile data:", error);
+        res
+          .status(500)
+          .json({ error: "Failed to update facility data fields" });
+      }
+    });
+
+    
+    
+
+
+    // Ping diagnostic check deployment layer validation
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello Server World!! Server is running fine.');
+app.get("/", (req, res) => {
+  res.send("Server is running fine.");
 });
 
 app.listen(PORT, () => {
